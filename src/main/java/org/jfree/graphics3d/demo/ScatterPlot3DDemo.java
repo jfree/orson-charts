824fb9da9024d07db6c866b4aa72b0bb8de30506
David Gilbert <david.gilbert@object-refinery.com>
2013-07-23 07:12:25 +0200

    Clean-up.

--- src/main/java/org/jfree/graphics3d/demo/ScatterPlot3DDemo.java
+++ src/main/java/org/jfree/graphics3d/demo/ScatterPlot3DDemo.java
@@ -1,8 +1,6 @@
 package org.jfree.graphics3d.demo;
 
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -44,37 +42,13 @@ public class ScatterPlot3DDemo extends JFrame {
   JPanel createContent() {
     JPanel content = new JPanel(new BorderLayout());
     content.setPreferredSize(new Dimension(600, 400));
-
-    //World world = new World();
-
-    //ChartBox3D chartBox = new ChartBox3D(10, 10, 10, 0.0, 0.0, 0.0, new Color(0, 0, 255, 200));
-    //world.addObject(chartBox.getObject3D());
-
-    Axis3D xAxis = new Axis3D(new Range(0.0, 10.0));
-    xAxis.setLineStroke(new BasicStroke(3.0f));
-    xAxis.setLinePaint(Color.RED);
-    Axis3D yAxis = new Axis3D(new Range(0.0, 20.0));
-    yAxis.setLineStroke(new BasicStroke(3.0f));
-    yAxis.setLinePaint(Color.GREEN);
-    Axis3D zAxis = new Axis3D(new Range(0.0, 30.0));
-    zAxis.setLineStroke(new BasicStroke(3.0f));
-    zAxis.setLinePaint(Color.BLUE);
+    Axis3D xAxis = new Axis3D("X", new Range(0.0, 10.0));
+    Axis3D yAxis = new Axis3D("Y", new Range(0.0, 20.0));
+    Axis3D zAxis = new Axis3D("Z", new Range(0.0, 30.0));
     XYZPlot plot = new XYZPlot(xAxis, yAxis, zAxis);
     plot.setDataset(new DefaultXYZDataset(3, 100));
     plot.setRenderer(new ScatterXYZRenderer());
     this.chartPanel3D = new ChartPanel3D(new JFreeChart3D(plot));
-
-//        World axisWorld = new World();
-//        axisWorld.addObject(new Dot3D(-3.0f, -2.0f, -2.0f, Color.YELLOW));
-//        axisWorld.addObject(new Dot3D(-3.0f, 2.0f, -2.0f, Color.YELLOW));
-//        axisWorld.addObject(new Dot3D(3.0f, -2.0f, -2.0f, Color.YELLOW));
-//        axisWorld.addObject(new Dot3D(3.0f, 2.0f, -2.0f, Color.YELLOW));
-//
-//        axisWorld.addObject(new Dot3D(-3.0f, -2.0f, 2.0f, Color.YELLOW));
-//        axisWorld.addObject(new Dot3D(-3.0f, 2.0f, 2.0f, Color.YELLOW));
-//        axisWorld.addObject(new Dot3D(3.0f, -2.0f, 2.0f, Color.YELLOW));
-//        axisWorld.addObject(new Dot3D(3.0f, 2.0f, 2.0f, Color.YELLOW));
-//        this.panel3D.setOverlayWorld(axisWorld);
     content.add(this.chartPanel3D);
 
     return content;
@@ -86,7 +60,7 @@ public class ScatterPlot3DDemo extends JFrame {
    * @param args  command line arguments (ignored).
    */
   public static void main(String[] args) {
-    ScatterPlot3DDemo app = new ScatterPlot3DDemo("Graphics3D Demo");
+    ScatterPlot3DDemo app = new ScatterPlot3DDemo("JFreeChart3D : ScatterPlot3DDemo1.java");
     app.pack();
     app.setVisible(true);
   }
