cc5653376a17066a4a1a8dd838a2db633e070350
David Gilbert <david.gilbert@object-refinery.com>
2013-07-23 07:11:03 +0200

    Rotate axis value labels.

--- src/main/java/org/jfree/chart3d/axis/Axis3D.java
+++ src/main/java/org/jfree/chart3d/axis/Axis3D.java
@@ -5,6 +5,7 @@ package org.jfree.chart3d.axis;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Paint;
 import java.awt.Stroke;
@@ -18,10 +19,17 @@ import java.text.NumberFormat;
  */
 public class Axis3D {
 
+  /** The axis label (if null, no label is displayed). */
+  private String label;
+  
+  private Font labelFont;
+ 
   private Range range;
 
   private double tickSize;
 
+  private Font valueLabelFont = new Font("SansSerif", Font.PLAIN, 12);
+  
   private Stroke lineStroke;
 
   private Paint linePaint;
@@ -31,12 +39,30 @@ public class Axis3D {
   /**
    * Creates a new axis with the specified range.
    *
+   * @param label  the axis label (<code>null</code> permitted).
    * @param range  the range (<code>null</code> not permitted).
    */
-  public Axis3D(Range range) {
+  public Axis3D(String label, Range range) {
+    this.label = label;
+    this.labelFont = new Font("SansSerif", Font.BOLD, 12);
     this.range = range;
     this.tickFormatter = new DecimalFormat("0.00");
     this.tickSize = range.getLength() / 10.0;  // FIXME
+    this.lineStroke = new BasicStroke(1.0f);
+    this.linePaint = Color.GRAY;
+  }
+  
+  public String getLabel() {
+    return this.label;
+  }
+  
+  /**
+   * Sets the axis label.
+   * 
+   * @param label  the label (<code>null</code> permitted). 
+   */
+  public void setLabel(String label) {
+    this.label = label;
   }
 
   public double getTickSize() {
@@ -62,9 +88,10 @@ public class Axis3D {
    * @param g2
    * @param pt0
    * @param pt1
+   * @param opposingPt
+   * @param labels
    */
   public void render(Graphics2D g2, Point2D pt0, Point2D pt1, Point2D opposingPt, boolean labels) {
-    // g2.getRenderingHints().put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g2.setStroke(this.lineStroke);
     g2.setPaint(this.linePaint);
     Line2D axisLine = new Line2D.Float(pt0, pt1);
@@ -74,40 +101,85 @@ public class Axis3D {
     // point to the side where the text labels will be displayed
     // we could do this by assuming that the diagonally opposite
     // line segment in the cube is on the "inside" of the chart
+    g2.setFont(this.valueLabelFont);
     for (int i = 0; i <= 10; i++) {
       Line2D perpLine = createPerpendicularLine(axisLine, 0.1 * i, 10.0, opposingPt);
       g2.setPaint(Color.BLACK);
       g2.setStroke(new BasicStroke(1f));
       g2.draw(perpLine);
+      double theta = calculateTheta(axisLine);
+      double thetaAdj = theta + Math.PI / 2.0;
+      if (thetaAdj < -Math.PI / 2.0) {
+          thetaAdj = thetaAdj + Math.PI;
+      }
+      if (thetaAdj > Math.PI / 2.0) {
+          thetaAdj = thetaAdj - Math.PI;
+      }
       double v = range.getMin() + (0.1 * i * range.getLength());
-      TextUtils.drawAlignedString(this.tickFormatter.format(v), g2, (float) perpLine.getX2(), (float) perpLine.getY2(), TextAnchor.CENTER);
+
+      double perpTheta = calculateTheta(perpLine);
+      TextAnchor textAnchor = TextAnchor.CENTER_LEFT;
+      if (Math.abs(perpTheta) > Math.PI / 2.0) {
+          textAnchor = TextAnchor.CENTER_RIGHT;
+      } 
+      TextUtils.drawRotatedString(this.tickFormatter.format(v), g2, (float) perpLine.getX2(), (float) perpLine.getY2(), textAnchor,
+              thetaAdj, textAnchor);
       //  g2.drawString(String.valueOf(i), (float) perpLine.getX2(), (float) perpLine.getY2());
     }
 
+    if (this.label != null) {
+      g2.setFont(this.labelFont);
+      Line2D labelPosLine = createPerpendicularLine(axisLine, 0.5, 60.0, 
+              opposingPt);
+      double theta = calculateTheta(axisLine);
+      if (theta < -Math.PI / 2.0) {
+          theta = theta + Math.PI;
+      }
+      if (theta > Math.PI / 2.0) {
+          theta = theta - Math.PI;
+      }
+      TextUtils.drawRotatedString(this.label, g2, (float) labelPosLine.getX2(), 
+              (float) labelPosLine.getY2(), TextAnchor.CENTER, theta, TextAnchor.CENTER);
+    }
   }
 
-    private Line2D createPerpendicularLine(Line2D line, double b, double size) {
-        double dx = line.getX2() - line.getX1();
-        double dy = line.getY2() - line.getY1();
-        double length = Math.sqrt(dx * dx + dy * dy);
-        double pdx = dy / length;
-        double pdy = -dx / length;
-        Point2D pt1 = new Point2D.Double(line.getX1() + b * dx, line.getY1() + b * dy);
-        Point2D pt2 = new Point2D.Double(pt1.getX() + size * pdx, pt1.getY() + size * pdy);
-        return new Line2D.Double(pt1, pt2);
-    }
+  private static final double EPSILON = 0.000001;
+  
+  private double calculateTheta(Line2D line) {
+      double dx = line.getX2() - line.getX1();
+      double dy = line.getY2() - line.getY1();
+      double theta = Math.atan2(dy, dx);
+      return theta;
+  }
+  
+  /**
+   * 
+   */
+  private Line2D createPerpendicularLine(Line2D line, double b, double size) {
+    double dx = line.getX2() - line.getX1();
+    double dy = line.getY2() - line.getY1();
+    double length = Math.sqrt(dx * dx + dy * dy);
+    double pdx = dy / length;
+    double pdy = -dx / length;
+    Point2D pt1 = new Point2D.Double(line.getX1() + b * dx, line.getY1() + b * dy);
+    Point2D pt2 = new Point2D.Double(pt1.getX() + size * pdx, pt1.getY() + size * pdy);
+    return new Line2D.Double(pt1, pt2);
+  }
 
   /**
    * Creates and returns a line that is perpendicular to the specified line.
    *
    * @param line  the reference line.
-   * @param b  the base point, expressed as a percentage along the length of the reference line.
+   * @param b  the base point, expressed as a percentage along the length of 
+   *     the reference line.
    * @param size  the size or length of the perpendicular line.
-   * @param opposingPoint  an opposing point, to define which side of the reference line the perpendicular line will extend.
+   * @param opposingPoint  an opposing point, to define which side of the 
+   *     reference line the perpendicular line will extend.
    *
    * @return The perpendicular line.
    */
-  private Line2D createPerpendicularLine(Line2D line, double b, double size, Point2D opposingPoint) {
+  private Line2D createPerpendicularLine(Line2D line, double b, double size, 
+          Point2D opposingPoint) {
     double dx = line.getX2() - line.getX1();
     double dy = line.getY2() - line.getY1();
     double length = Math.sqrt(dx * dx + dy * dy);
