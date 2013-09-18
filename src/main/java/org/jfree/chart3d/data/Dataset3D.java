8f7db806bb26689f4966c96ca73a4a5771ac233e
David Gilbert <david.gilbert@object-refinery.com>
2013-09-18 14:01:41 +0200

    Added Javadocs.

--- src/main/java/org/jfree/chart3d/data/Dataset3D.java
+++ src/main/java/org/jfree/chart3d/data/Dataset3D.java
@@ -1,6 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * (C)opyright 2013, by Object Refinery Limited
  */
 package org.jfree.chart3d.data;
 
@@ -11,8 +10,21 @@ import org.jfree.chart3d.event.Dataset3DChangeListener;
  * the change event notification mechanism.
  */
 public interface Dataset3D {
-    
+
+  /**
+   * Registers a change listener to receive notification of changes to the
+   * dataset.
+   * 
+   * @param listener  the listener (<code>null</code> not permitted). 
+   */
   void addChangeListener(Dataset3DChangeListener listener);  
+  
+  /**
+   * De-registers a change listener so that it no longer receives notification
+   * of changes to the dataset.
+   * 
+   * @param listener  the listener (<code>null</code> not permitted). 
+   */
   void removeChangeListener(Dataset3DChangeListener listener);  
 
 }
