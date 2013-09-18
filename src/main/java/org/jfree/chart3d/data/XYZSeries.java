6d3c4813ec9de954c0a9b3713df23805884dbf2f
David Gilbert <david.gilbert@object-refinery.com>
2013-09-18 14:04:08 +0200

    Additional methods.

--- src/main/java/org/jfree/chart3d/data/XYZSeries.java
+++ src/main/java/org/jfree/chart3d/data/XYZSeries.java
@@ -12,22 +12,53 @@ import org.jfree.graphics3d.ArgChecks;
  */
 public class XYZSeries {
 
-  private String name;
+  private Comparable key;
 
   private List<XYZDataItem> items;
 
-  public XYZSeries(String name) {
-    this.name = name;
+  /**
+   * Creates a new series with the specified key.
+   * 
+   * @param key  the key (<code>null</code> not permitted). 
+   */
+  public XYZSeries(Comparable key) {
+    ArgChecks.nullNotPermitted(key, "key");
+    this.key = key;
     this.items = new ArrayList<XYZDataItem>();
   }
 
+  /**
+   * Returns the number of items in the series.
+   * 
+   * @return The number of items in the series. 
+   */
   public int getItemCount() {
     return this.items.size();
   }
 
+  public void add(double x, double y, double z) {
+    add(new XYZDataItem(x, y, z));   
+  }
+
+  /**
+   * Adds an item to the series.
+   * 
+   * @param item 
+   */
   public void add(XYZDataItem item) {
     ArgChecks.nullNotPermitted(item, "item");
     this.items.add(item);
   }
 
+  public double getXValue(int itemIndex) {
+    return this.items.get(itemIndex).getX();
+  }
+  
+  public double getYValue(int itemIndex) {
+    return this.items.get(itemIndex).getY();
+  }
+
+  public double getZValue(int itemIndex) {
+    return this.items.get(itemIndex).getZ();
+  }
 }
