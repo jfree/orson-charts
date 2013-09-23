18973fd1b226365007c51368ba3c5e43b3ac008d
David Gilbert <david.gilbert@object-refinery.com>
2013-09-23 11:50:44 +0200

    Added getDoubleValue() method.

--- src/main/java/com/orsoncharts/data/Values3D.java
+++ src/main/java/com/orsoncharts/data/Values3D.java
@@ -5,41 +5,58 @@ package com.orsoncharts.data;
 
 /**
  * A generic representation of a three dimensional grid (cube) of data values.
+ * We refer to the indices in the three dimensions as the seriesIndex, the
+ * rowIndex and the columnIndex (to match the downstream use of this data
+ * structure to represent data values in a three dimensional plot).
  * 
  * @see KeyedValues3D
  */
 public interface Values3D {
 
-  /**
-   * Returns the number of items in the x-dimension.
-   * 
-   * @return The number of items in the x-dimension. 
-   */
-  public int getXCount();
+    /**
+     * Returns the number of items in the x-dimension.
+     * 
+     * @return The number of items in the x-dimension. 
+     */
+    public int getSeriesCount();
   
-  /**
-   * Returns the number of items in the y-dimension.
-   * 
-   * @return The number of items in the y-dimension. 
-   */
-  public int getYCount();
+    /**
+     * Returns the number of items in the y-dimension.
+     * 
+     * @return The number of items in the y-dimension. 
+     */
+    public int getRowCount();
   
-  /**
-   * Returns the number of items in the z-dimension.
-   * 
-   * @return The number of items in the z-dimension. 
-   */
-  public int getZCount();
+    /**
+     * Returns the number of items in the z-dimension.
+     * 
+     * @return The number of items in the z-dimension. 
+     */
+    public int getColumnCount();
   
-  /**
-   * Returns the data item at the specified position.
-   * 
-   * @param xIndex  the x-index.
-   * @param yIndex  the y-index.
-   * @param zIndex  the z-index.
-   * 
-   * @return The data value (possibly <code>null</code>).
-   */
-  public Number getValue(int xIndex, int yIndex, int zIndex);
+    /**
+     * Returns the data item at the specified position.
+     * 
+     * @param seriesIndex  the series-index.
+     * @param rowIndex  the row-index.
+     * @param columnIndex  the column-index.
+     * 
+     * @return The data value (possibly <code>null</code>).
+     */
+    public Number getValue(int seriesIndex, int rowIndex, int columnIndex);
+   
+    /**
+     * Returns the data item at the specified position as a double primitive.
+     * Where the {@link #getValue(int, int)} method returns <code>null</code>, 
+     * this method returns <code>Double.NaN</code>.
+     * 
+     * @param seriesIndex  the series index.
+     * @param rowIndex  the row index.
+     * @param columnIndex  the column index.
+     * 
+     * @return The data value.
+     */
+    public double getDoubleValue(int seriesIndex, int rowIndex, 
+            int columnIndex);
   
 }
