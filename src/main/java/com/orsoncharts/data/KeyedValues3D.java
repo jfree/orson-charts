5f947bb4f29c4f4d2e963028075e7d510761a88c
David Gilbert <david.gilbert@object-refinery.com>
2013-09-23 11:55:19 +0200

    Method name changes and Javadoc additions.

--- src/main/java/com/orsoncharts/data/KeyedValues3D.java
+++ src/main/java/com/orsoncharts/data/KeyedValues3D.java
@@ -7,21 +7,55 @@ import java.util.List;
 
 /**
  * A three dimensional cube of data values where each value is uniquely 
- * identified by three keys (the xKey, yKey and zKey).
+ * identified by three keys (the seriesKey, rowKey and columnKey).  
  */
 public interface KeyedValues3D extends Values3D {
 
-  public Comparable getXKey(int xIndex);
-  public Comparable getYKey(int yIndex);
-  public Comparable getZKey(int zIndex);
+    /**
+     * Returns a list of the series-keys for the dataset.  Modifying this
+     * list will have no impact on the underlying dataset.
+     * 
+     * @return A list of the series-keys (possibly empty, but never 
+     *     <code>null</code>). 
+     */
+    public List<Comparable> getSeriesKeys();
 
-  public int getXIndex(Comparable xkey);
-  public int getYIndex(Comparable ykey);
-  public int getZIndex(Comparable zkey);
-  
-  public List<Comparable> getXKeys();
-  public List<Comparable> getYKeys();
-  public List<Comparable> getZKeys();
+    /**
+     * Returns a list of the row-keys for the dataset.  Modifying this
+     * list will have no impact on the underlying dataset.
+     * 
+     * @return A list of the row-keys (possibly empty, but never 
+     *     <code>null</code>). 
+     */
+    public List<Comparable> getRowKeys();
+    
+    /**
+     * Returns a list of the column-keys for the dataset.  Modifying this
+     * list will have no impact on the underlying dataset.
+     * 
+     * @return A list of the column-keys (possibly empty, but never 
+     *     <code>null</code>). 
+     */
+    public List<Comparable> getColumnKeys();
+    
+    public Comparable getSeriesKey(int seriesIndex);
+    public Comparable getRowKey(int rowIndex);
+    public Comparable getColumnKey(int columnIndex);
 
-  public Number getValue(Comparable xKey, Comparable yKey, Comparable zKey);
+    public int getSeriesIndex(Comparable serieskey);
+    public int getRowIndex(Comparable rowkey);
+    public int getColumnIndex(Comparable columnkey);
+
+    /**
+     * Returns the value for a given series, row and column.
+     * 
+     * @param seriesKey the series key (<code>null</code> not permitted).
+     * @param rowKey  the row key (<code>null</code> not permitted).
+     * @param columnKey  the column key (<code>null</code> not permitted).
+     * 
+     * @return The value (possibly <code>null</code>).
+     */
+    public Number getValue(Comparable seriesKey, Comparable rowKey, 
+            Comparable columnKey);
+    
 }
