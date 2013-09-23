7bc2e3770d0dea9fa3dba820ba614f0436ac002a
David Gilbert <david.gilbert@object-refinery.com>
2013-09-23 11:58:57 +0200

    Implement with tests.

--- src/main/java/com/orsoncharts/data/DefaultKeyedValues3D.java
+++ src/main/java/com/orsoncharts/data/DefaultKeyedValues3D.java
@@ -13,100 +13,147 @@ import java.util.List;
  */
 public class DefaultKeyedValues3D implements KeyedValues3D {
 
-  private List<Comparable> xKeys;
+    private List<Comparable> seriesKeys;
   
-  private List<Comparable> yKeys;
+    private List<Comparable> rowKeys;
   
-  private List<Comparable> zKeys;
+    private List<Comparable> columnKeys;
 
-  private List<DefaultKeyedValues2D> data;
+    /**
+     * The data, one entry per series.  Each series *must* contain the same
+     * row and column keys.
+     */
+    private List<DefaultKeyedValues2D> data; // one entry per series
   
-  /**
-   * Creates a new (empty) table.
-   */
-  public DefaultKeyedValues3D() {
-    this.xKeys = new ArrayList<Comparable>();
-    this.yKeys = new ArrayList<Comparable>();
-    this.zKeys = new ArrayList<Comparable>();
-    this.data = new ArrayList<DefaultKeyedValues2D>();
-  }
+    /**
+     * Creates a new (empty) table.
+     */
+    public DefaultKeyedValues3D() {
+        this.seriesKeys = new ArrayList<Comparable>();
+        this.rowKeys = new ArrayList<Comparable>();
+        this.columnKeys = new ArrayList<Comparable>();
+        this.data = new ArrayList<DefaultKeyedValues2D>();
+    }
   
-  @Override
-  public Comparable getXKey(int xIndex) {
-    return this.xKeys.get(xIndex);
-  }
+    @Override
+    public Comparable getSeriesKey(int seriesIndex) {
+        return this.seriesKeys.get(seriesIndex);
+    }
 
-  @Override
-  public Comparable getYKey(int yIndex) {
-    return this.yKeys.get(yIndex);
-  }
+    @Override
+    public Comparable getRowKey(int rowIndex) {
+        return this.rowKeys.get(rowIndex);
+    }
 
-  @Override
-  public Comparable getZKey(int zIndex) {
-    return this.zKeys.get(zIndex);
-  }
+    @Override
+    public Comparable getColumnKey(int columnIndex) {
+        return this.columnKeys.get(columnIndex);
+    }
 
-  @Override
-  public int getXIndex(Comparable xkey) {
-    return this.xKeys.indexOf(xkey);
-  }
+    @Override
+    public int getSeriesIndex(Comparable seriesKey) {
+        return this.seriesKeys.indexOf(seriesKey);
+    }
 
-  @Override
-  public int getYIndex(Comparable ykey) {
-    return this.yKeys.indexOf(ykey);
-  }
+    @Override
+    public int getRowIndex(Comparable rowKey) {
+        return this.rowKeys.indexOf(rowKey);
+    }
 
-  @Override
-  public int getZIndex(Comparable zkey) {
-    return this.zKeys.indexOf(zkey);
-  }
+    @Override
+    public int getColumnIndex(Comparable columnKey) {
+        return this.columnKeys.indexOf(columnKey);
+    }
 
-  @Override
-  public List<Comparable> getXKeys() {
-    return Collections.unmodifiableList(this.xKeys);
-  }
+    @Override
+    public List<Comparable> getSeriesKeys() {
+        return Collections.unmodifiableList(this.seriesKeys);
+    }
 
-  @Override
-  public List<Comparable> getYKeys() {
-    return Collections.unmodifiableList(this.yKeys);
-  }
+    @Override
+    public List<Comparable> getRowKeys() {
+        return Collections.unmodifiableList(this.rowKeys);
+    }
 
-  @Override
-  public List<Comparable> getZKeys() {
-    return Collections.unmodifiableList(this.zKeys);
-  }
+    @Override
+    public List<Comparable> getColumnKeys() {
+        return Collections.unmodifiableList(this.columnKeys);
+    }
 
-  @Override
-  public int getXCount() {
-    return this.xKeys.size();
-  }
+    @Override
+    public int getSeriesCount() {
+        return this.seriesKeys.size();
+    }
 
-  @Override
-  public int getYCount() {
-    return this.yKeys.size();
-  }
+    @Override
+    public int getRowCount() {
+        return this.rowKeys.size();
+    }
 
-  @Override
-  public int getZCount() {
-    return this.zKeys.size();
-  }
+    @Override
+    public int getColumnCount() {
+        return this.columnKeys.size();
+    }
 
-  @Override
-  public Number getValue(int xIndex, int yIndex, int zIndex) {
-    return this.data.get(zIndex).getValue(xIndex, yIndex);
-  }
+    @Override
+    public Number getValue(int seriesIndex, int rowIndex, int columnIndex) {
+        return this.data.get(seriesIndex).getValue(rowIndex, columnIndex);
+    }
     
-  @Override
-  public Number getValue(Comparable xKey, Comparable yKey, Comparable zKey) {
-    return getValue(getXIndex(xKey), getYIndex(yKey), getZIndex(zKey));
-  }
+    @Override
+    public Number getValue(Comparable seriesKey, Comparable rowKey, 
+            Comparable columnKey) {
+        return getValue(getSeriesIndex(seriesKey), getRowIndex(rowKey), 
+                getColumnIndex(columnKey));
+    }
 
-  public void addValue(Number n, Comparable xKey, Comparable yKey, 
-          Comparable zKey) {
-      // cases:
-      // 1 - the dataset is empty, so we just need to add a new layer with the
-      //     given keys;
-      // 2 - the three keys already exist -> just update the value
-      // 3 - complicated part...
-  }
+    @Override
+    public double getDoubleValue(int seriesIndex, int rowIndex, int columnIndex) {
+        Number n = getValue(seriesIndex, rowIndex, columnIndex);
+        if (n != null) {
+            return n.doubleValue();
+        }
+        return Double.NaN;
+    }
+    
+    public void setValue(Number n, Comparable seriesKey, Comparable rowKey, 
+            Comparable columnKey) {
+        // cases:
+        // 1 - the dataset is empty, so we just need to add a new layer with the
+        //     given keys;
+        if (this.data.isEmpty()) {
+            this.seriesKeys.add(seriesKey);
+            this.rowKeys.add(rowKey);
+            this.columnKeys.add(columnKey);
+            DefaultKeyedValues2D d = new DefaultKeyedValues2D();
+            d.setValue(n, rowKey, columnKey);
+            this.data.add(d);
+        }
+        
+        int seriesIndex = getSeriesIndex(seriesKey);
+        int rowIndex = getRowIndex(rowKey);
+        int columnIndex = getColumnIndex(columnKey);
+        if (rowIndex < 0) {
+            this.rowKeys.add(rowKey);
+        }
+        if (columnIndex < 0) {
+            this.columnKeys.add(columnKey);
+        }
+        if (rowIndex < 0 || columnIndex < 0) {
+            for (DefaultKeyedValues2D d : this.data) {
+                d.setValue(null, rowKey, columnKey);
+            } 
+        } 
+        if (seriesIndex >= 0) {
+            DefaultKeyedValues2D d = this.data.get(seriesIndex);
+            d.setValue(n, rowKey, columnKey);
+        } else {
+            this.seriesKeys.add(seriesKey);
+            DefaultKeyedValues2D d = new DefaultKeyedValues2D(this.rowKeys, 
+                    this.columnKeys);
+            d.setValue(n, rowKey, columnKey);
+            this.data.add(d);
+        }
+    }
+
 }
