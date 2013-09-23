c4b92f5eb646337d7aabf404657857bba984efad
David Gilbert <david.gilbert@object-refinery.com>
2013-09-23 11:54:26 +0200

    Removed old header text.

--- src/main/java/com/orsoncharts/axis/TextAnchor.java
+++ src/main/java/com/orsoncharts/axis/TextAnchor.java
@@ -1,42 +1,5 @@
-/* ===========================================================
- * JFreeChart : a free chart library for the Java(tm) platform
- * ===========================================================
- *
- * (C) Copyright 2000-2012, by Object Refinery Limited and Contributors.
- *
- * Project Info:  http://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
- * Other names may be trademarks of their respective owners.]
- *
- * ---------------
- * TextAnchor.java
- * ---------------
- * (C) Copyright 2003-2012, by Object Refinery Limited.
- *
- * Original Author:  David Gilbert (for Object Refinery Limited);
- * Contributor(s):   -;
- *
- * Changes:
- * --------
- * 10-Jun-2003 : Version 1 (DG);
- * 11-Jan-2005 : Removed deprecated code (DG);
- *
+/**
+ * (C)opyright 2013, by Object Refinery Limited
  */
 
 package com.orsoncharts.axis;
