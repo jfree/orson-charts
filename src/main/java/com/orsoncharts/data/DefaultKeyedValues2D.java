949f4fa27786b5ad25c552b983114ecf21cafaec
David Gilbert <david.gilbert@object-refinery.com>
2013-09-23 11:57:12 +0200

    New implementation.

--- src/main/java/com/orsoncharts/data/DefaultKeyedValues2D.java
+++ src/main/java/com/orsoncharts/data/DefaultKeyedValues2D.java
@@ -3,6 +3,7 @@
  */
 package com.orsoncharts.data;
 
+import com.orsoncharts.graphics3d.ArgChecks;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,29 +16,78 @@ public class DefaultKeyedValues2D implements KeyedValues2D {
     
     List<Comparable> yKeys;
     
-    List<DefaultKeyedValues> data;
+    List<DefaultKeyedValues> data;  // one entry per xKey
   
+    /**
+     * Creates a new (empty) instance.
+     */
     public DefaultKeyedValues2D() {
-        this.xKeys = new ArrayList<Comparable>();
-        this.yKeys = new ArrayList<Comparable>();
+        this(new ArrayList<Comparable>(), new ArrayList<Comparable>());
+    }
+    
+    /**
+     * Creates a new instance with the specified keys and all data values 
+     * initialized to <code>null</code>.
+     * 
+     * @param xKeys  the xKeys (<code>null</code> not permitted).
+     * @param yKeys  the yKeys (<code>null</code> not permitted).
+     */
+    public DefaultKeyedValues2D(List<Comparable> xKeys, 
+            List<Comparable> yKeys) {
+        ArgChecks.nullNotPermitted(xKeys, "xKeys");
+        ArgChecks.nullNotPermitted(yKeys, "yKeys");
+        this.xKeys = new ArrayList<Comparable>(xKeys);
+        this.yKeys = new ArrayList<Comparable>(yKeys);
         this.data = new ArrayList<DefaultKeyedValues>();    
+        for (Comparable c : xKeys) {
+            this.data.add(new DefaultKeyedValues(yKeys));
+        }
     }
 
+    /**
+     * Returns the x-key corresponding to the specified index.
+     * 
+     * @param xIndex  the index.
+     * 
+     * @return The key. 
+     */
     @Override
     public Comparable getXKey(int xIndex) {
         return this.xKeys.get(xIndex);
     }
 
+    /**
+     * Returns the y-key corresponding to the specified index.
+     * 
+     * @param yIndex  the index.
+     * 
+     * @return The key. 
+     */
     @Override
     public Comparable getYKey(int yIndex) {
         return this.yKeys.get(yIndex);
     }
 
+    /**
+     * Returns the index corresponding to the specified x-key.
+     * 
+     * @param xkey  the x-key (<code>null</code> not permitted).
+     * 
+     * @return The index. 
+     */
     @Override
     public int getXIndex(Comparable xkey) {
+        ArgChecks.nullNotPermitted(xkey, "xkey");
         return this.xKeys.indexOf(xkey);
     }
 
+    /**
+     * Returns the index corresponding to the specified y-key.
+     * 
+     * @param ykey  the y-key (<code>null</code> not permitted).
+     * 
+     * @return The index. 
+     */
     @Override
     public int getYIndex(Comparable ykey) {
         return this.yKeys.indexOf(ykey);
@@ -45,13 +95,19 @@ public class DefaultKeyedValues2D implements KeyedValues2D {
 
     /**
      * Returns a copy of the list of xKeys.
-     * @return 
+     * 
+     * @return A copy of the list of xKeys (never <code>null</code>). 
      */
     @Override
     public List<Comparable> getXKeys() {
         return new ArrayList(this.xKeys);
     }
 
+    /**
+     * Returns a copy of the list of y-keys.
+     * 
+     * @return A copy of the list of y-keys (never <code>null</code>). 
+     */
     @Override
     public List<Comparable> getYKeys() {
         return new ArrayList<Comparable>(this.yKeys);
@@ -78,5 +134,62 @@ public class DefaultKeyedValues2D implements KeyedValues2D {
     public Number getValue(int xIndex, int yIndex) {
         return this.data.get(xIndex).getValue(yIndex);
     }
+
+    @Override
+    public double getDoubleValue(int xIndex, int yIndex) {
+        Number n = getValue(xIndex, yIndex);
+        if (n != null) {
+            return n.doubleValue();
+        }
+        return Double.NaN;
+    } 
     
+    public void setValue(Number n, Comparable xKey, Comparable yKey) {
+        // cases
+        
+        // 1.  No data - just add one new entry
+        if (this.data.isEmpty()) {
+            this.xKeys.add(xKey);
+            this.yKeys.add(yKey);
+            DefaultKeyedValues dkvs = new DefaultKeyedValues();
+            dkvs.addValue(yKey, n);
+            this.data.add(dkvs);
+        } else {
+            int xIndex = getXIndex(xKey);
+            int yIndex = getYIndex(yKey);
+            if (xIndex >= 0) {
+                DefaultKeyedValues dkvs = this.data.get(xIndex);
+                if (yIndex >= 0) {
+                    // 2.  Both keys exist - just update the value
+                    dkvs.addValue(yKey, n);
+                } else {
+                    // 3.  xKey exists, but yKey does not (add the yKey to each series)
+                    this.yKeys.add(yKey);
+                    for (DefaultKeyedValues kv : this.data) {
+                        kv.addValue(yKey, null);
+                    }
+                    dkvs.addValue(yKey, n);
+                }
+            } else {
+                if (yIndex >= 0) {
+                    // 4.  xKey does not exist, but yKey does
+                    this.xKeys.add(xKey);
+                    DefaultKeyedValues d = new DefaultKeyedValues(this.yKeys);
+                    d.addValue(yKey, n);
+                    this.data.add(d);
+                } else {
+                    // 5.  neither key exists
+                    // need to create the new series, plus the new entry in every series
+                    this.xKeys.add(xKey);
+                    this.yKeys.add(yKey);
+                    for (DefaultKeyedValues kv : this.data) {
+                        kv.addValue(yKey, null);
+                    }
+                    DefaultKeyedValues d = new DefaultKeyedValues(this.yKeys);
+                    d.addValue(yKey, n);
+                    this.data.add(d);
+                }
+            }
+        }
+    }
 }
