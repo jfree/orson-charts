20f03c6afc5a8cc119a802029462f4c14447feb1
David Gilbert <david.gilbert@object-refinery.com>
2013-09-23 11:55:00 +0200

    Add size property, and use paint source.

--- src/main/java/com/orsoncharts/renderer/ScatterXYZRenderer.java
+++ src/main/java/com/orsoncharts/renderer/ScatterXYZRenderer.java
@@ -16,48 +16,64 @@ import com.orsoncharts.graphics3d.World;
  * A renderer for 3D scatter plots.
  */
 public class ScatterXYZRenderer extends AbstractXYZRenderer 
-    implements XYZRenderer {
+        implements XYZRenderer {
 
-  public ScatterXYZRenderer() {
+    private double size;
     
-  }
+    /**
+     * Creates a new instance.
+     */
+    public ScatterXYZRenderer() {
+        super();
+        this.size = 0.05;
+    }
 
-  public Paint getItemPaint(int series, int item) {
-    if (series == 0)
-      return Color.RED;
-    if (series == 1)
-        return Color.GREEN;
-    if (series == 2)
-        return Color.BLUE;
-    return Color.GRAY;
-  }
-
-  @Override
-  public void composeItem(XYZDataset dataset, int series, int item, 
-      World world, Dimension3D dimensions, double xOffset, double yOffset, 
-      double zOffset) {
-    
-    double x = dataset.getX(series, item);
-    double y = dataset.getY(series, item);
-    double z = dataset.getZ(series, item);
-    
-    XYZPlot plot = getPlot();
-    Axis3D xAxis = plot.getXAxis();
-    Axis3D yAxis = plot.getYAxis();
-    Axis3D zAxis = plot.getZAxis();
-    if (!xAxis.getRange().contains(x) || !yAxis.getRange().contains(y) 
-            || !zAxis.getRange().contains(z)) {
-      return;
+    /**
+     * Returns the size of the cubes used to display each data item.  The 
+     * default value is 0.05.
+     * 
+     * @return The size.
+     */
+    public double getSize() {
+        return this.size;
     }
     
-    Dimension3D dim = plot.getDimensions();
-    double xx = xAxis.translateToWorld(x, dim.getWidth());
-    double yy = yAxis.translateToWorld(y, dim.getHeight());
-    double zz = zAxis.translateToWorld(z, dim.getDepth());
+    /**
+     * Sets the size of the cubes used to represent each data item.
+     * 
+     * @param size  the size.
+     */
+    public void setSize(double size) {
+        this.size = size; 
+    }
     
-    Paint paint = getItemPaint(series, item);
-    Object3D cube = Object3D.createCube(0.1, xx + xOffset, yy + yOffset, zz + zOffset, (Color) paint);
-    world.add(cube);
-  }
+    @Override
+    public void composeItem(XYZDataset dataset, int series, int item, 
+        World world, Dimension3D dimensions, double xOffset, double yOffset, 
+        double zOffset) {
+    
+        double x = dataset.getX(series, item);
+        double y = dataset.getY(series, item);
+        double z = dataset.getZ(series, item);
+    
+        XYZPlot plot = getPlot();
+        Axis3D xAxis = plot.getXAxis();
+        Axis3D yAxis = plot.getYAxis();
+        Axis3D zAxis = plot.getZAxis();
+        if (!xAxis.getRange().contains(x) || !yAxis.getRange().contains(y) 
+              || !zAxis.getRange().contains(z)) {
+            return;
+        }
+    
+        Dimension3D dim = plot.getDimensions();
+        double xx = xAxis.translateToWorld(x, dim.getWidth());
+        double yy = yAxis.translateToWorld(y, dim.getHeight());
+        double zz = zAxis.translateToWorld(z, dim.getDepth());
+    
+        Paint paint = getPaintSource().getPaint(series, item);
+        Object3D cube = Object3D.createCube(this.size, xx + xOffset, 
+                yy + yOffset, zz + zOffset, (Color) paint);
+        world.add(cube);
+    }
 
 }
