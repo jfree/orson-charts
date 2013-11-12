ee081451189e2690b67c9cfe6775647bc2964de2
David Gilbert <david.gilbert@object-refinery.com>
2013-11-12 15:44:26 +0100

    Implement write to PNG.

--- src/main/java/com/orsoncharts/graphics3d/swing/ExportToPNGAction.java
+++ src/main/java/com/orsoncharts/graphics3d/swing/ExportToPNGAction.java
@@ -8,7 +8,15 @@
 
 package com.orsoncharts.graphics3d.swing;
 
+import com.orsoncharts.util.ArgChecks;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.geom.Dimension2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -21,15 +29,20 @@ public class ExportToPNGAction extends AbstractAction {
     /** The panel to which this action applies. */
     private Panel3D panel;
   
-    public ExportToPNGAction(Panel3D panel3D) {
+    /**
+     * Creates a new action instance.
+     * 
+     * @param panel  the panel (<code>null</code> not permitted).
+     */
+    public ExportToPNGAction(Panel3D panel) {
         super("PNG...");
-        this.panel = panel3D;
+        ArgChecks.nullNotPermitted(panel, "panel");
+        this.panel = panel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         JFileChooser fileChooser = new JFileChooser();
-        //fileChooser.setCurrentDirectory(this.defaultDirectoryForSaveAs);
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
                     "PNG Image Files", "png");
         fileChooser.addChoosableFileFilter(filter);
@@ -40,6 +53,18 @@ public class ExportToPNGAction extends AbstractAction {
             String filename = fileChooser.getSelectedFile().getPath();
             if (!filename.endsWith(".png")) {
                 filename = filename + ".png";
+            }
+            Dimension2D size = this.panel.getSize();
+            int w = (int) size.getWidth();
+            int h = (int) size.getHeight();
+            BufferedImage image = new BufferedImage(w, h, 
+                    BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = image.createGraphics();
+            this.panel.getDrawable().draw(g2, new Rectangle(w, h));
+            try {
+                ImageIO.write(image, "png", new File(filename));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         }
     }
