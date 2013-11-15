861c03995968c7fa97f6e778ec9f90a19b89eb98
David Gilbert <david.gilbert@object-refinery.com>
2013-11-15 14:55:11 +0100

    Make non-eval version.

--- src/main/java/com/orsoncharts/Chart3D.java
+++ src/main/java/com/orsoncharts/Chart3D.java
@@ -558,7 +558,7 @@ public class Chart3D implements Drawable3D, Plot3DChangeListener, Serializable {
         // generate and draw the legend...
         if (this.legendBuilder != null) {
             TableElement legend = this.legendBuilder.createLegend(this.plot);
-            if (true) { // eval
+            if (false) { // eval
                 GridElement legend2 = new GridElement();
                 legend2.setElement(legend, "R1", "C1");
                 TextElement te = new TextElement("Orson Charts (evaluation) (c) 2013, by Object Refinery Limited", new Font("Dialog", Font.PLAIN, 10));
