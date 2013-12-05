7867f6ca30449be18edab9f1625a5b08380ddc27
David Gilbert <david.gilbert@object-refinery.com>
2013-12-05 12:17:56 +0100

    Add new tests.

--- src/test/java/com/orsoncharts/plot/StandardColorSourceTest.java
+++ src/test/java/com/orsoncharts/plot/StandardColorSourceTest.java
@@ -8,8 +8,10 @@
 
 package com.orsoncharts.plot;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.awt.Color;
 import org.junit.Test;
@@ -18,6 +20,31 @@ import org.junit.Test;
  * Some tests for the {@link StandardColorSource} class.
  */
 public class StandardColorSourceTest {
+
+    @Test
+    public void testConstructors() {
+        StandardColorSource scs = new StandardColorSource(Color.RED);
+        assertEquals(Color.RED, scs.getColor("A"));
+        
+        scs = new StandardColorSource(Color.RED, Color.GREEN, Color.BLUE);
+        assertEquals(Color.RED, scs.getColor("A"));
+        assertEquals(Color.GREEN, scs.getColor("B"));
+        assertEquals(Color.BLUE, scs.getColor("C"));
+        
+        try {
+            scs = new StandardColorSource((Color[]) null);
+            fail("Should have thrown an exception.");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+        
+        try {
+            scs = new StandardColorSource(Color.RED, null);
+            fail("Should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
 
     @Test
     public void testEquals() {
