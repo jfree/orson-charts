877462b0b6eac6fe0402e7dadd3c7503ea19d9bd
David Gilbert <david.gilbert@object-refinery.com>
2013-12-11 12:07:11 +0100

    New file used in legend layout.

---
+++ src/main/java/com/orsoncharts/util/Orientation.java
@@ -0,0 +1,25 @@
+/* ============
+ * Orson Charts
+ * ============
+ * 
+ * (C)opyright 2013, by Object Refinery Limited.
+ * 
+ */
+
+package com.orsoncharts.util;
+
+/**
+ * General enumeration of orientations.  One application for this is to 
+ * specify whether the legend should be created for a vertical or horizontal
+ * layout.
+ * 
+ * @since 1.1
+ */
+public enum Orientation {
+    
+    /** Horizontal orientation. */
+    HORIZONTAL,
+    
+    /** Vertical orientation. */
+    VERTICAL
+}
