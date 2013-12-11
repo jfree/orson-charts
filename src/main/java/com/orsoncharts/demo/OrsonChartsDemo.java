bbc80d6be0dfc5edf9a9fd97b1766a3f82485f30
David Gilbert <david.gilbert@object-refinery.com>
2013-12-11 14:13:20 +0100

    Add AxisRangeDemo6.

--- src/main/java/com/orsoncharts/demo/OrsonChartsDemo.java
+++ src/main/java/com/orsoncharts/demo/OrsonChartsDemo.java
@@ -290,6 +290,8 @@ public class OrsonChartsDemo extends JFrame implements ActionListener,
                 "AxisRangeDemo4.java"));
         n.add(createNode("com.orsoncharts.demo.AxisRangeDemo5", 
                 "AxisRangeDemo5.java"));
+        n.add(createNode("com.orsoncharts.demo.AxisRangeDemo6", 
+                "AxisRangeDemo6.java"));
         return n;                
     }
 
