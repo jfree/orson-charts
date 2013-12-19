65000d6fe30ae630559d315711ba0585028ad0c4
David Gilbert <david.gilbert@object-refinery.com>
2013-12-19 09:02:42 +0100

    Use ExitOnClose.

--- src/main/java/com/orsoncharts/demo/ScatterPlot3DDemo2.java
+++ src/main/java/com/orsoncharts/demo/ScatterPlot3DDemo2.java
@@ -9,8 +9,6 @@
 package com.orsoncharts.demo;
 
 import java.awt.BorderLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -38,12 +36,7 @@ public class ScatterPlot3DDemo2 extends JFrame {
      */
     public ScatterPlot3DDemo2(String title) {
         super(title);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        addWindowListener(new ExitOnClose());
         getContentPane().add(createDemoPanel());
     }
 
