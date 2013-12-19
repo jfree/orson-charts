8686c7ea88d7f47ab9aa36a7806bd555481f0edf
David Gilbert <david.gilbert@object-refinery.com>
2013-12-19 16:15:34 +0100

    Minor Javadoc edit.

--- src/main/java/com/orsoncharts/axis/package-info.java
+++ src/main/java/com/orsoncharts/axis/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Axes ({@link com.orsoncharts.axis.CategoryAxis3D} and 
- * {@link com.orsoncharts.axis.NumberAxis3D}) and supporting classes and 
+ * {@link com.orsoncharts.axis.NumberAxis3D}) plus supporting classes and 
  * interfaces.
  */
 package com.orsoncharts.axis;
