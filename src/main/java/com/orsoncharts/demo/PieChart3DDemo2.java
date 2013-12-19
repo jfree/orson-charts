cf2d06a5f9e41c2efe6a5a66793a070099880663
David Gilbert <david.gilbert@object-refinery.com>
2013-12-19 09:03:07 +0100

    Use ExitOnClose.

--- src/main/java/com/orsoncharts/demo/PieChart3DDemo2.java
+++ src/main/java/com/orsoncharts/demo/PieChart3DDemo2.java
@@ -11,8 +11,6 @@ package com.orsoncharts.demo;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -95,12 +93,7 @@ public class PieChart3DDemo2 extends JFrame {
      */
     public PieChart3DDemo2(String title) {
         super(title);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        addWindowListener(new ExitOnClose());
         getContentPane().add(createDemoPanel());
     }
 
