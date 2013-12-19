8a0fa1e7b7c4faa0900080ad4ba7e983850664e6
David Gilbert <david.gilbert@object-refinery.com>
2013-12-19 09:07:43 +0100

    Use ExitOnClose.

--- src/main/java/com/orsoncharts/demo/StackedBarChart3DDemo2.java
+++ src/main/java/com/orsoncharts/demo/StackedBarChart3DDemo2.java
@@ -9,8 +9,6 @@
 package com.orsoncharts.demo;
 
 import java.awt.BorderLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import com.orsoncharts.ChartPanel3D;
@@ -37,12 +35,7 @@ public class StackedBarChart3DDemo2 extends JFrame {
      */
     public StackedBarChart3DDemo2(String title) {
         super(title);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        addWindowListener(new ExitOnClose());
         getContentPane().add(createDemoPanel());
     }
 
