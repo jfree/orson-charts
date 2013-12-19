303920ec280a00529ab46a92eb9cc53df8a3b8e7
David Gilbert <david.gilbert@object-refinery.com>
2013-12-19 08:54:30 +0100

    Use ExitOnClose.

--- src/main/java/com/orsoncharts/demo/AxisRangeDemo1.java
+++ src/main/java/com/orsoncharts/demo/AxisRangeDemo1.java
@@ -11,8 +11,6 @@ package com.orsoncharts.demo;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
 import javax.swing.JLabel;
@@ -79,12 +77,7 @@ public class AxisRangeDemo1 extends JFrame {
      */
     public AxisRangeDemo1(String title) {
         super(title);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        addWindowListener(new ExitOnClose());
         getContentPane().add(createDemoPanel());
     }
 
