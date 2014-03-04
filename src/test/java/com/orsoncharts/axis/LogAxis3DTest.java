5d8b01d42933baa7649fd2928f2872f2700b3d92
David Gilbert <david.gilbert@object-refinery.com>
2014-03-04 09:39:08 +0100

    Some tests for the LogAxis3D class.

---
+++ src/test/java/com/orsoncharts/axis/LogAxis3DTest.java
@@ -0,0 +1,183 @@
+/* ============
+ * Orson Charts
+ * ============
+ * 
+ * (C)opyright 2013, 2014, by Object Refinery Limited.
+ * 
+ * http://www.object-refinery.com/orsoncharts/index.html
+ * 
+ * Redistribution of this source file is prohibited.
+ * 
+ */
+
+package com.orsoncharts.axis;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.text.DecimalFormat;
+
+import org.junit.Test;
+
+import com.orsoncharts.TestUtils;
+
+/**
+ * Tests for the {@link LogAxis3D} class.
+ */
+public class LogAxis3DTest {
+    
+    @Test
+    public void testEquals() {
+        LogAxis3D axis1 = new LogAxis3D("Label");   
+        LogAxis3D axis2 = new LogAxis3D("Label"); 
+        assertTrue(axis1.equals(axis2));
+        assertFalse(axis1.equals(null));
+        
+        // axis label
+        axis1.setLabel("Label2");
+        assertFalse(axis1.equals(axis2));
+        axis2.setLabel("Label2");
+        assertTrue(axis1.equals(axis2));
+        
+        // axis label can be null also
+        axis1.setLabel(null);
+        assertFalse(axis1.equals(axis2));
+        axis2.setLabel(null);
+        assertTrue(axis1.equals(axis2));
+
+        // label font
+        axis1.setLabelFont(new Font("Dialog", Font.PLAIN, 2));
+        assertFalse(axis1.equals(axis2));
+        axis2.setLabelFont(new Font("Dialog", Font.PLAIN, 2));
+        assertTrue(axis1.equals(axis2));
+        
+        // label color
+        axis1.setLabelColor(Color.RED);
+        assertFalse(axis1.equals(axis2));
+        axis2.setLabelColor(Color.RED);
+        assertTrue(axis1.equals(axis2));
+        
+        // line stroke
+        axis1.setLineStroke(new BasicStroke(0.5f));
+        assertFalse(axis1.equals(axis2));
+        axis2.setLineStroke(new BasicStroke(0.5f));
+        assertTrue(axis1.equals(axis2));
+        
+        // line color
+        axis1.setLineColor(Color.BLUE);
+        assertFalse(axis1.equals(axis2));
+        axis2.setLineColor(Color.BLUE);
+        assertTrue(axis1.equals(axis2));
+        
+        // tickLabelsVisible
+        axis1.setTickLabelsVisible(false);
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickLabelsVisible(false);
+        assertTrue(axis1.equals(axis2));
+        
+        // tick label font
+        axis1.setTickLabelFont(new Font("Dialog", Font.PLAIN, 5));
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickLabelFont(new Font("Dialog", Font.PLAIN, 5));
+        assertTrue(axis1.equals(axis2));
+        
+        // tick label color
+        axis1.setTickLabelColor(Color.GREEN);
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickLabelColor(Color.GREEN);
+        assertTrue(axis1.equals(axis2));
+
+        // axis visible
+        axis1.setVisible(false);
+        assertFalse(axis1.equals(axis2));
+        axis2.setVisible(false);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setLowerMargin(0.123);
+        assertFalse(axis1.equals(axis2));
+        axis2.setLowerMargin(0.123);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setUpperMargin(0.321);
+        assertFalse(axis1.equals(axis2));
+        axis2.setUpperMargin(0.321);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickMarkLength(123);
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickMarkLength(123);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickMarkPaint(new GradientPaint(1f, 2f, Color.BLUE, 3f, 4f,
+                Color.GREEN));
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickMarkPaint(new GradientPaint(1f, 2f, Color.BLUE, 3f, 4f,
+                Color.GREEN));
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickMarkStroke(new BasicStroke(1.23f));
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickMarkStroke(new BasicStroke(1.23f));
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setBase(5.0);
+        assertFalse(axis1.equals(axis2));
+        axis2.setBase(5.0);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setSmallestValue(10.0);
+        assertFalse(axis1.equals(axis2));
+        axis2.setSmallestValue(10.0);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setBaseSymbol("x");
+        assertFalse(axis1.equals(axis2));
+        axis2.setBaseSymbol("x");
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setBaseFormatter(new DecimalFormat("0.00"));
+        assertFalse(axis1.equals(axis2));
+        axis2.setBaseFormatter(new DecimalFormat("0.00"));
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickSelector(null);
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickSelector(null);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickSize(12.3);
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickSize(12.3);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickLabelFormatter(new DecimalFormat("0.000"));
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickLabelFormatter(new DecimalFormat("0.000"));
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickLabelFactor(1.2);
+        assertFalse(axis1.equals(axis2));
+        axis2.setTickLabelFactor(1.2);
+        assertTrue(axis1.equals(axis2));
+    }
+    
+    /**
+     * Checks for serialization support.
+     */
+    @Test
+    public void testSerialization() {
+        LogAxis3D axis1 = new LogAxis3D("T");
+        LogAxis3D axis2 = (LogAxis3D) TestUtils.serialized(axis1);
+        assertTrue(axis1.equals(axis2));
+        
+        axis1.setTickMarkPaint(new GradientPaint(9f, 10f, Color.PINK, 11f, 12f,
+                Color.BLACK));
+        axis2 = (LogAxis3D) TestUtils.serialized(axis1);
+        assertTrue(axis1.equals(axis2));
+    }
+
+}
