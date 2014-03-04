778a0b49fbdc4dd013c6f3f4808bfff99bd0b537
David Gilbert <david.gilbert@object-refinery.com>
2014-03-04 09:34:51 +0100

    New enum.

---
+++ src/main/java/com/orsoncharts/axis/LabelOrientation.java
@@ -0,0 +1,26 @@
+/* ============
+ * Orson Charts
+ * ============
+ * 
+ * (C)opyright 2013, 2014, by Object Refinery Limited.
+ * 
+ * http://www.object-refinery.com/orsoncharts/index.html
+ * 
+ * Redistribution of this source file is prohibited.
+ * 
+ */
+
+package com.orsoncharts.axis;
+
+/**
+ * The orientation of an axis label relative to the axis.
+ */
+public enum LabelOrientation {
+    
+    /** Labels are drawn perpendicular to the axis line. */
+    PERPENDICULAR,
+    
+    /** Labels are drawn parallel to the axis line. */
+    PARALLEL
+    
+}
