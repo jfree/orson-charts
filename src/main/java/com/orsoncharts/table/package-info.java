bfa815c871fcbbab92957b029b818fdc845c1f0e
David Gilbert <david.gilbert@object-refinery.com>
2014-03-18 10:45:20 +0100

    Javadoc update.

--- src/main/java/com/orsoncharts/table/package-info.java
+++ src/main/java/com/orsoncharts/table/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Classes and interfaces for constructing tables, used for chart legend
- * construction.
+ * Classes and interfaces for constructing tables for display to the user.
+ * This is used for chart legend construction, but the API is intended to be
+ * general purpose and should not contain any chart-specific code.
  */
 package com.orsoncharts.table;
