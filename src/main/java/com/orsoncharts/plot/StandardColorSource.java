6e3e556874891eae96374d8e98c8ed443754cbcd
David Gilbert <david.gilbert@object-refinery.com>
2014-03-31 11:51:23 +0200

    Fix NB warning.

--- src/main/java/com/orsoncharts/plot/StandardColorSource.java
+++ src/main/java/com/orsoncharts/plot/StandardColorSource.java
@@ -60,8 +60,8 @@ public final class StandardColorSource implements ColorSource,
             throw new IllegalArgumentException(
                     "Zero length array not permitted.");
         }
-        for (int i = 0; i < colors.length; i++) {
-            if (colors[i] == null) {
+        for (Color c: colors) {
+            if (c == null) { 
                 throw new IllegalArgumentException(
                         "Null array entries not permitted.");
             }
