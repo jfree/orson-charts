a8be193050e8da7fef770e4825e9a9e0c6014745
David Gilbert <david.gilbert@object-refinery.com>
2014-04-04 12:14:43 +0200

    Update test for modified KeyedValues2D interface.

--- src/test/java/com/orsoncharts/data/DefaultKeyedValues2DTest.java
+++ src/test/java/com/orsoncharts/data/DefaultKeyedValues2DTest.java
@@ -12,13 +12,14 @@
 
 package com.orsoncharts.data;
 
-import com.orsoncharts.TestUtils;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
+import com.orsoncharts.TestUtils;
+import com.orsoncharts.data.category.StandardCategoryDataset3D;
 
 /**
  * Some checks for the {@link DefaultKeyedValues2D} class.
@@ -28,40 +29,57 @@ public class DefaultKeyedValues2DTest {
     private static final double EPSILON = 0.00000001;
     
     @Test
+    public void testGetValue() {
+        DefaultKeyedValues2D<Number> kv2d = new DefaultKeyedValues2D<Number>();
+        kv2d.setValue(1.0, "R1", "C1");
+        kv2d.setValue(2.0, "R1", "C2");
+        kv2d.setValue(3.0, "R1", "C3");
+        kv2d.setValue(4.0, "R2", "C1");
+        kv2d.setValue(5.0, "R2", "C2");
+        kv2d.setValue(6.0, "R2", "C3");
+
+        assertEquals(1.0, kv2d.getValue("R1", "C1"));
+        assertEquals(2.0, kv2d.getValue("R1", "C2"));
+        assertEquals(3.0, kv2d.getValue("R1", "C3"));
+        assertEquals(4.0, kv2d.getValue("R2", "C1"));
+        assertEquals(5.0, kv2d.getValue("R2", "C2"));
+        assertEquals(6.0, kv2d.getValue("R2", "C3"));
+    }
+    
+    @Test
     public void testSetValue() {
-        
         // empty data
         DefaultKeyedValues2D<Number> data = new DefaultKeyedValues2D<Number>();
-        data.setValue(1.0, "X1", "Y1");
-        assertEquals(1.0, data.getValue("X1", "Y1").doubleValue(), EPSILON);
-        assertEquals(1, data.getXCount());
-        assertEquals(1, data.getYCount());
+        data.setValue(1.0, "R1", "C1");
+        assertEquals(1.0, data.getValue("R1", "C1").doubleValue(), EPSILON);
+        assertEquals(1, data.getRowCount());
+        assertEquals(1, data.getColumnCount());
         
         // existing keys
-        data.setValue(2.0, "X1", "Y1");
-        assertEquals(2.0, data.getValue("X1", "Y1").doubleValue(), EPSILON);
-        assertEquals(1, data.getXCount());
-        assertEquals(1, data.getYCount());
+        data.setValue(2.0, "R1", "C1");
+        assertEquals(2.0, data.getValue("R1", "C1").doubleValue(), EPSILON);
+        assertEquals(1, data.getRowCount());
+        assertEquals(1, data.getColumnCount());
 
-        // existing x-key, new y-key
-        data.setValue(3.0, "X1", "Y2");
-        assertEquals(3.0, data.getValue("X1", "Y2").doubleValue(), EPSILON);
-        assertEquals(1, data.getXCount());
-        assertEquals(2, data.getYCount());
+        // existing row key, new column key
+        data.setValue(3.0, "R1", "C2");
+        assertEquals(3.0, data.getValue("R1", "C2").doubleValue(), EPSILON);
+        assertEquals(1, data.getRowCount());
+        assertEquals(2, data.getColumnCount());
         
-        // existing y-key, new x-key
-        data.setValue(4.0, "X2", "Y2");
-        assertEquals(4.0, data.getValue("X2", "Y2").doubleValue(), EPSILON);
-        assertEquals(2, data.getXCount());
-        assertEquals(2, data.getYCount());
-        assertNull(data.getValue("X2", "Y1"));
+        // existing column key, new row key
+        data.setValue(4.0, "R2", "C2");
+        assertEquals(4.0, data.getValue("R2", "C2").doubleValue(), EPSILON);
+        assertEquals(2, data.getRowCount());
+        assertEquals(2, data.getColumnCount());
+        assertNull(data.getValue("R2", "C1"));
         
-        // new x-key and new y-key
-        data.setValue(5.0, "X3", "Y3");
-        assertEquals(5.0, data.getValue("X3", "Y3").doubleValue(), EPSILON);
-        assertEquals(3, data.getXCount());
-        assertEquals(3, data.getYCount());
-        assertNull(data.getValue("X1", "Y3"));
+        // new row key and new column key
+        data.setValue(5.0, "R3", "C3");
+        assertEquals(5.0, data.getValue("R3", "C3").doubleValue(), EPSILON);
+        assertEquals(3, data.getRowCount());
+        assertEquals(3, data.getColumnCount());
+        assertNull(data.getValue("R1", "C3"));
     }
     
     @Test
@@ -71,9 +89,9 @@ public class DefaultKeyedValues2DTest {
         assertTrue(d1.equals(d2));
         assertFalse(d1.equals(null));
         
-        d1.setValue(1.0, "X1", "Y1");
+        d1.setValue(1.0, "R1", "C1");
         assertFalse(d1.equals(d2));
-        d2.setValue(1.0, "X1", "Y1");
+        d2.setValue(1.0, "R1", "C1");
         assertTrue(d1.equals(d2));
     }
     
