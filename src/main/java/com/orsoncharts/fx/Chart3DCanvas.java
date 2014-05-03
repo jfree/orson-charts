057646d210a7d629e5a67c4bf9603b9e0dd788cc
David Gilbert <david.gilbert@object-refinery.com>
2014-05-03 07:12:21 +0200

    Added tooltip support.

--- src/main/java/com/orsoncharts/fx/Chart3DCanvas.java
+++ src/main/java/com/orsoncharts/fx/Chart3DCanvas.java
@@ -12,17 +12,24 @@
 
 package com.orsoncharts.fx;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.geom.Dimension2D;
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import com.orsoncharts.Chart3D;
+import com.orsoncharts.data.ItemKey;
 import com.orsoncharts.fx.graphics2d.FXGraphics2D;
+import com.orsoncharts.graphics3d.Dimension3D;
+import com.orsoncharts.graphics3d.Object3D;
 import com.orsoncharts.graphics3d.Offset2D;
+import com.orsoncharts.graphics3d.RenderingInfo;
 import com.orsoncharts.graphics3d.ViewPoint3D;
+import javafx.scene.control.Tooltip;
 
 /**
  * A canvas for displaying a {@link Chart3D} in JavaFX. 
@@ -38,7 +45,12 @@ public class Chart3DCanvas extends Canvas {
 
     private double maxViewingDistanceMultiplier;
     
+    private double margin = 0.25;
+    
     private Graphics2D g2;
+
+    /** The rendering info from the last drawing of the chart. */
+    private RenderingInfo renderingInfo;
 
     /** The angle increment for panning left and right (in radians). */
     private double panIncrement;
@@ -66,6 +78,8 @@ public class Chart3DCanvas extends Canvas {
      */
     private Offset2D offsetAtMousePressed;
     
+    private Tooltip tooltip;
+    
     /**
      * Creates a new canvas to display the supplied chart in JavaFX.
      * 
@@ -92,23 +106,19 @@ public class Chart3DCanvas extends Canvas {
                 Chart3DCanvas.this.draw();
             }
         });
-        setOnMousePressed(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent me) {
-                Chart3DCanvas.this.lastClickPoint = new Point((int) me.getScreenX(), (int) me.getScreenY());
-                Chart3DCanvas.this.lastMovePoint = Chart3DCanvas.this.lastClickPoint;
-                //this.offsetAtMousePressed = this.drawable.getTranslate2D();
-            }
+        setOnMouseMoved((MouseEvent me) -> { updateTooltip(me); });
+        setOnMousePressed((MouseEvent me) -> {
+            Chart3DCanvas.this.lastClickPoint = new Point((int) me.getScreenX(), (int) me.getScreenY());
+            Chart3DCanvas.this.lastMovePoint = Chart3DCanvas.this.lastClickPoint;
         });
-        setOnMouseDragged(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent me) {
-                Point currPt = new Point((int) me.getScreenX(), (int) me.getScreenY());
-                int dx = currPt.x - Chart3DCanvas.this.lastMovePoint.x;
-                int dy = currPt.y - Chart3DCanvas.this.lastMovePoint.y;
-                Chart3DCanvas.this.lastMovePoint = currPt;
-                Chart3DCanvas.this.chart.getViewPoint().panLeftRight(-dx * Math.PI / 120);
-                Chart3DCanvas.this.chart.getViewPoint().moveUpDown(-dy * Math.PI / 120);            
-                Chart3DCanvas.this.draw();
-            }
+        setOnMouseDragged((MouseEvent me) -> {
+            Point currPt = new Point((int) me.getScreenX(), (int) me.getScreenY());
+            int dx = currPt.x - Chart3DCanvas.this.lastMovePoint.x;
+            int dy = currPt.y - Chart3DCanvas.this.lastMovePoint.y;
+            Chart3DCanvas.this.lastMovePoint = currPt;
+            Chart3DCanvas.this.chart.getViewPoint().panLeftRight(-dx * Math.PI / 120);
+            Chart3DCanvas.this.chart.getViewPoint().moveUpDown(-dy * Math.PI / 120);
+            Chart3DCanvas.this.draw();
         });
         
     }
@@ -122,12 +132,34 @@ public class Chart3DCanvas extends Canvas {
         return this.g2;
     }
  
+    /**
+     * Adjusts the viewing distance so that the chart fits the specified
+     * size.  A margin is left (see {@link #getMargin()} around the edges to 
+     * leave room for labels etc.
+     * 
+     * @param size  the target size (<code>null</code> not permitted).
+     */    
+    public void zoomToFit(double width, double height) {
+        int w = (int) (width * (1.0 - this.margin));
+        int h = (int) (height * (1.0 - this.margin));
+        Dimension2D target = new Dimension(w, h);
+        Dimension3D d3d = this.chart.getDimensions();
+        float distance = this.chart.getViewPoint().optimalDistance(target, 
+                d3d, this.chart.getProjDistance());
+        this.chart.getViewPoint().setRho(distance);
+        draw();        
+    }
+
+    /**
+     * Draws the content of the canvas.
+     */
     public void draw() {
         getGraphicsContext2D().save();
         double width = getWidth();
         double height = getHeight();
         if (width > 0 && height > 0) {
-            this.chart.draw(this.g2, new Rectangle((int) width, (int) height));
+            this.renderingInfo = this.chart.draw(this.g2, 
+                    new Rectangle((int) width, (int) height));
         }
         getGraphicsContext2D().restore();
     }
@@ -147,5 +179,30 @@ public class Chart3DCanvas extends Canvas {
         return 100;
     }
 
+    protected void updateTooltip(MouseEvent me) {
+        if (this.renderingInfo != null) {
+            Object3D object = this.renderingInfo.fetchObjectAt(me.getX(), 
+                    me.getY());
+            if (object != null) {
+                ItemKey key = (ItemKey) object.getProperty(Object3D.ITEM_KEY);
+                if (key != null) {
+                    String toolTipText = chart.getPlot().generateToolTipText(key);
+                    if (this.tooltip == null) {
+                        this.tooltip = new Tooltip(toolTipText);
+                        Tooltip.install(this, this.tooltip);
+                    } else {
+                        this.tooltip.setText(toolTipText);
+                        this.tooltip.setAnchorX(me.getScreenX());
+                        this.tooltip.setAnchorY(me.getScreenY());
+                    }                   
+                } else {
+                    if (this.tooltip != null) {
+                        Tooltip.uninstall(this, this.tooltip);
+                    }
+                    this.tooltip = null;
+                }
+            }
+        }
+    }
 }
 
