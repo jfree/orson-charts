a6ab21d8b92f03c9e5e00952af11f7061fe35288
David Gilbert <david.gilbert@object-refinery.com>
2014-05-16 19:40:03 +0200

    Additional tests for NumberTickSelector.

--- src/test/java/com/orsoncharts/axis/NumberTickSelectorTest.java
+++ src/test/java/com/orsoncharts/axis/NumberTickSelectorTest.java
@@ -14,8 +14,10 @@ package com.orsoncharts.axis;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import com.orsoncharts.TestUtils;
+import static org.junit.Assert.fail;
 
 /**
  * Tests for the {@link NumberTickSelector} class.
@@ -42,5 +44,41 @@ public class NumberTickSelectorTest {
         NumberTickSelector s1 = new NumberTickSelector();
         NumberTickSelector s2 = (NumberTickSelector) TestUtils.serialized(s1);
         assertTrue(s1.equals(s2));
+    }
+    
+    public static final double EPSILON = 0.0000001;
+    
+    @Test
+    public void testSelect() {
+        NumberTickSelector s = new NumberTickSelector();
+        assertEquals(1.0, s.getCurrentTickSize(), EPSILON);
+        double d = s.getCurrentTickSize();
+        s.select(10);
+        assertEquals(s.getCurrentTickSize(), 10.0, EPSILON);
+        s.select(14);
+        assertEquals(s.getCurrentTickSize(), 100.0, EPSILON);
+        s.select(1000000000000000000.0);
+        assertEquals(s.getCurrentTickSize(), 1000000000000000000.0, EPSILON);
+        
+        try {
+            s.select(0);
+            fail("Expected an IllegalArgumentException for this case.");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+        
+        try {
+            s.select(Double.POSITIVE_INFINITY);
+            fail("Expected an IllegalArgumentException for this case.");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+
+        try {
+            s.select(Double.NEGATIVE_INFINITY);
+            fail("Expected an IllegalArgumentException for this case.");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
     }
 }
