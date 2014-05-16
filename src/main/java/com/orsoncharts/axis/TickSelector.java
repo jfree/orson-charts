49b9dfa8e35b8265539527c148522f7d6b2b56e3
David Gilbert <david.gilbert@object-refinery.com>
2014-05-16 19:31:16 +0200

    Filled out Javadocs.

--- src/main/java/com/orsoncharts/axis/TickSelector.java
+++ src/main/java/com/orsoncharts/axis/TickSelector.java
@@ -15,17 +15,35 @@ package com.orsoncharts.axis;
 import java.text.Format;
 
 /**
- * Provides standard tick sizes and formatting for numerical axes.
+ * <p>Provides standard tick sizes and formatting for numerical axes.  
+ * Conceptually, the selector maintains a list of standard tick sizes (ordered 
+ * by size), and a pointer to the current (selected) tick size.  Clients of
+ * the selector will initialise the pointer by calling the 
+ * {@link #select(double)} method with a reference value (a guess, based on 
+ * context, of the largest usable tick size - for example, one half of the
+ * axis length) then, as required, move the pointer to a smaller or larger
+ * tick size (using the {@link #next()} and {@link #previous()} methods) to 
+ * find the most appropriate standard size to use.</p>
+ * 
+ * <p>The {@link NumberTickSelector} class provides a standard implementation, 
+ * but you can create your own if necessary.</p>
  */
 public interface TickSelector {
     
     /**
-     * Selects a standard tick unit as close as possible to the reference 
-     * value.
+     * Selects and returns a standard tick size that is greater than or equal to 
+     * the specified reference value and, ideally, as close to it as possible 
+     * (to minimise the number of iterations used by axes to determine the tick
+     * size to use).  After a call to this method, the 
+     * {@link #getCurrentTickSize()} method should return the selected tick 
+     * size (there is a "cursor" that points to this tick size), the 
+     * {@link #next()} method should move the pointer to the next (larger) 
+     * standard tick size, and the {@link #previous()} method should move the 
+     * pointer to the  previous (smaller) standard tick size.
      * 
-     * @param reference  the reference value.
+     * @param reference  the reference value (must be positive and finite).
      * 
-     * @return The standard tick unit.
+     * @return The selected tick size. 
      */
     double select(double reference);
     
