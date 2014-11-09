2a686d50cc6a4eeb0209f49c763c1df00c76b4d5
David Gilbert <dave@jfree.org>
2014-11-09 12:31:46 +0100

    Add checkbox to switch axis between log axis and regular axis.

--- src/main/java/com/orsoncharts/demo/fx/ScatterPlot3DFXDemo2.java
+++ src/main/java/com/orsoncharts/demo/fx/ScatterPlot3DFXDemo2.java
@@ -42,10 +42,20 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import com.orsoncharts.Chart3D;
+import com.orsoncharts.axis.LabelOrientation;
+import com.orsoncharts.axis.LogAxis3D;
+import com.orsoncharts.axis.NumberAxis3D;
 import com.orsoncharts.data.xyz.XYZDataset;
 import com.orsoncharts.demo.ScatterPlot3D2;
 import com.orsoncharts.fx.Chart3DViewer;
+import com.orsoncharts.plot.XYZPlot;
+import com.orsoncharts.style.ChartStyler;
 
 /**
  * A scatter plot demo for JavaFX.
@@ -56,7 +66,30 @@ public class ScatterPlot3DFXDemo2 extends Application {
         XYZDataset dataset = ScatterPlot3D2.createDataset();
         Chart3D chart = ScatterPlot3D2.createChart(dataset);
         Chart3DViewer viewer = new Chart3DViewer(chart);
-        return viewer;
+        BorderPane node = new BorderPane();
+        node.setCenter(viewer);
+        HBox container = new HBox();
+        CheckBox checkBox = new CheckBox("Logarithmic Axis?");
+        checkBox.setSelected(true);
+        checkBox.setOnAction((e) -> {
+            XYZPlot plot = (XYZPlot) chart.getPlot();
+            if (checkBox.isSelected()) {
+                LogAxis3D logAxis = new LogAxis3D("Y (log scale)");
+                logAxis.setTickLabelOrientation(LabelOrientation.PERPENDICULAR);
+                logAxis.receive(new ChartStyler(chart.getStyle()));
+                plot.setYAxis(logAxis);
+            } else {
+                NumberAxis3D yAxis = new NumberAxis3D("Y");
+                yAxis.setTickLabelOrientation(LabelOrientation.PERPENDICULAR);
+                yAxis.receive(new ChartStyler(chart.getStyle()));
+                plot.setYAxis(yAxis);
+            }
+        });
+        container.setAlignment(Pos.CENTER);
+        container.setPadding(new Insets(4.0, 4.0, 4.0, 4.0));
+        container.getChildren().add(checkBox);
+        node.setBottom(container);
+        return node;
     }
 
     @Override
