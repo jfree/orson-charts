480d5cdcc8078897fe120177a81905593c392e19
David Gilbert <dave@jfree.org>
2015-01-16 17:19:46 +0100

    Add GPL3 as a license option.

--- src/main/java/com/orsoncharts/style/ChartStyle.java
+++ src/main/java/com/orsoncharts/style/ChartStyle.java
@@ -1,12 +1,32 @@
-/* ============
- * Orson Charts
- * ============
+/* ===========================================================
+ * Orson Charts : a 3D chart library for the Java(tm) platform
+ * ===========================================================
  * 
- * (C)opyright 2013, 2014, by Object Refinery Limited.
+ * (C)opyright 2013-2015, by Object Refinery Limited.  All rights reserved.
  * 
  * http://www.object-refinery.com/orsoncharts/index.html
  * 
- * Redistribution of this source file is prohibited.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ * 
+ * If you do not wish to be bound by the terms of the GPL, an alternative
+ * commercial license can be purchased.  For details, please see visit the
+ * Orson Charts home page:
+ * 
+ * http://www.object-refinery.com/orsoncharts/index.html
  * 
  */
 
