271ac09886f74d5c2925575a4d53d5451ea5a9ef
David Gilbert <dave@jfree.org>
2015-01-16 16:45:27 +0100

    Fix compile error.

--- src/main/java/com/orsoncharts/fx/Chart3DViewer.java
+++ src/main/java/com/orsoncharts/fx/Chart3DViewer.java
@@ -308,7 +308,7 @@ public class Chart3DViewer extends Control implements Skinnable {
     }
 
     @Override
-    protected String getUserAgentStylesheet() {
+    public String getUserAgentStylesheet() {
         return Chart3DViewer.class.getResource("chart3d-viewer.css")
                 .toExternalForm();
     }
