6805b1b45460023fc6e90a640a21a5b7708b06fe
David Gilbert <dave@jfree.org>
2015-11-24 11:47:00 +0100

    Javadoc updates.

--- src/main/java/com/orsoncharts/plot/FontSource.java
+++ src/main/java/com/orsoncharts/plot/FontSource.java
@@ -35,7 +35,7 @@ package com.orsoncharts.plot;
 import java.awt.Font;
 
 /**
- * An object that supplies <code>Font</code> instances associated with
+ * An object that supplies {@code Font} instances associated with
  * keys.  This is used by the {@link PiePlot3D} class to obtain section label
  * fonts for each data item(segment) in the chart.  A default implementation
  * ({@link StandardFontSource}) is provided.
@@ -45,17 +45,17 @@ public interface FontSource {
     /**
      * Returns a font based on the supplied key.
      * 
-     * @param key  the key (<code>null</code> not permitted).
+     * @param key  the key ({@code null} not permitted).
      * 
-     * @return A font (never <code>null</code>). 
+     * @return A font (never {@code null}). 
      */
     Font getFont(Comparable<?> key);
     
     /**
      * Sets the font associated with a key.
      * 
-     * @param key  the key (<code>null</code> not permitted).
-     * @param font  the font (<code>null</code> not permitted).
+     * @param key  the key ({@code null} not permitted).
+     * @param font  the font ({@code null} not permitted).
      */
     void setFont(Comparable<?> key, Font font);
     
@@ -65,7 +65,7 @@ public interface FontSource {
      * settings are cleared and this font is installed as the new default
      * section label font).
      * 
-     * @param font  the font (<code>null</code> not permitted). 
+     * @param font  the font ({@code null} not permitted). 
      * 
      * @since 1.2
      */
