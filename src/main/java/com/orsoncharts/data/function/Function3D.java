a627c5ad48016f528b0b6f54e45811d2ade6fe3a
David Gilbert <dave@jfree.org>
2015-11-24 07:58:47 +0100

    Javadoc updates.

--- src/main/java/com/orsoncharts/data/function/Function3D.java
+++ src/main/java/com/orsoncharts/data/function/Function3D.java
@@ -37,15 +37,15 @@ import com.orsoncharts.Range;
 import com.orsoncharts.data.xyz.XYZDatasetUtils;
 
 /**
- * Represents a function <code>y = f(x, z)</code>.  
+ * Represents a function {@code y = f(x, z)}.  
  * <br><br>
  * A dataset can be created by sampling a function - see the 
  * {@link XYZDatasetUtils#sampleFunction(Function3D, String, Range, double, Range, double)} 
- * method.  In addition, any <code>Function3D</code> instance can be plotted
+ * method.  In addition, any {@code Function3D} instance can be plotted
  * by a {@link com.orsoncharts.renderer.xyz.SurfaceRenderer}.
  */
 public interface Function3D extends Serializable {
-    
+
     /**
      * Returns the value of the function ('y') for the specified inputs ('x' 
      * and 'z').
@@ -56,5 +56,5 @@ public interface Function3D extends Serializable {
      * @return The function value.
      */
     public double getValue(double x, double z);
-   
+
 }
