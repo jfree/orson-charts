0a60c209cf5682c3d5841c1e1e5654a3b83e9bca
David Gilbert <dave@jfree.org>
2015-11-24 11:46:03 +0100

    Javadoc updates.

--- src/main/java/com/orsoncharts/plot/ColorSource.java
+++ src/main/java/com/orsoncharts/plot/ColorSource.java
@@ -44,11 +44,11 @@ public interface ColorSource {
 
     /**
      * Returns the color for one data item in the chart.  We return a 
-     * <code>Color</code> rather than a paint, because some manipulations
-     * that require a <code>Color</code> instance are done for the shading 
+     * {@code Color} rather than a paint, because some manipulations
+     * that require a {@code Color} instance are done for the shading 
      * during the 3D rendering.
      * 
-     * @param key  the key (<code>null</code> not permitted).
+     * @param key  the key ({@code null} not permitted).
      * 
      * @return The color.
      */
@@ -56,11 +56,11 @@ public interface ColorSource {
     
     /**
      * Sets the color associated with the specified key.  If the supplied
-     * color is <code>null</code>, this will have the effect of clearing any
+     * color is {@code null}, this will have the effect of clearing any
      * previous setting and reverting to the default color.
      * 
-     * @param key  the key (<code>null</code> not permitted).
-     * @param color  the color (<code>null</code> permitted).
+     * @param key  the key ({@code null} not permitted).
+     * @param color  the color ({@code null} permitted).
      */
     void setColor(Comparable<?> key, Color color);
     
