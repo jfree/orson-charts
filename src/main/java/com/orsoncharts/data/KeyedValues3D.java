8fcad66a66e34e0545ded397a9594ce87735f333
David Gilbert <dave@jfree.org>
2015-12-26 10:29:59 +0100

    Javadoc updates.

--- src/main/java/com/orsoncharts/data/KeyedValues3D.java
+++ src/main/java/com/orsoncharts/data/KeyedValues3D.java
@@ -36,8 +36,8 @@ import java.util.List;
 
 /**
  * A three dimensional cube of data values where each value is uniquely 
- * identified by three keys (the <code>seriesKey</code>, <code>rowKey</code> 
- * and <code>columnKey</code>).  
+ * identified by three keys (the {@code seriesKey}, {@code rowKey} 
+ * and {@code columnKey}).  
  */
 public interface KeyedValues3D<T> extends Values3D<T> {
 
@@ -46,7 +46,7 @@ public interface KeyedValues3D<T> extends Values3D<T> {
      * list will have no impact on the underlying dataset.
      * 
      * @return A list of the series keys (possibly empty, but never 
-     *     <code>null</code>). 
+     *     {@code null}). 
      */
     List<Comparable<?>> getSeriesKeys();
 
@@ -55,7 +55,7 @@ public interface KeyedValues3D<T> extends Values3D<T> {
      * list will have no impact on the underlying dataset.
      * 
      * @return A list of the row keys (possibly empty, but never 
-     *     <code>null</code>). 
+     *     {@code null}). 
      */
     List<Comparable<?>> getRowKeys();
     
@@ -64,7 +64,7 @@ public interface KeyedValues3D<T> extends Values3D<T> {
      * list will have no impact on the underlying dataset.
      * 
      * @return A list of the column keys (possibly empty, but never 
-     *     <code>null</code>). 
+     *     {@code null}). 
      */
     List<Comparable<?>> getColumnKeys();
     
@@ -96,43 +96,43 @@ public interface KeyedValues3D<T> extends Values3D<T> {
     Comparable<?> getColumnKey(int columnIndex);
 
     /**
-     * Returns the index of the specified series key, or <code>-1</code> if
+     * Returns the index of the specified series key, or {@code -1} if
      * there is no matching key.
      * 
-     * @param serieskey  the series key (<code>null</code> not permitted).
+     * @param serieskey  the series key ({@code null} not permitted).
      * 
-     * @return The key index, or <code>-1</code>. 
+     * @return The key index, or {@code -1}. 
      */
     int getSeriesIndex(Comparable<?> serieskey);
 
     /**
-     * Returns the index of the specified row key, or <code>-1</code> if there
+     * Returns the index of the specified row key, or {@code -1} if there
      * is no matching key.
      * 
-     * @param rowkey  the row key (<code>null</code> not permitted).
+     * @param rowkey  the row key ({@code null} not permitted).
      * 
-     * @return The row index or <code>-1</code>. 
+     * @return The row index or {@code -1}. 
      */
     int getRowIndex(Comparable<?> rowkey);
 
     /**
-     * Returns the index of the specified column key, or <code>-1</code> if 
+     * Returns the index of the specified column key, or {@code -1} if 
      * there is no matching key.
      * 
-     * @param columnkey  the column key (<code>null</code> not permitted).
+     * @param columnkey  the column key ({@code null} not permitted).
      * 
-     * @return The column index or <code>-1</code>. 
+     * @return The column index or {@code -1}. 
      */
     int getColumnIndex(Comparable<?> columnkey);
 
     /**
      * Returns the value for a given series, row and column.
      * 
-     * @param seriesKey the series key (<code>null</code> not permitted).
-     * @param rowKey  the row key (<code>null</code> not permitted).
-     * @param columnKey  the column key (<code>null</code> not permitted).
+     * @param seriesKey the series key ({@code null} not permitted).
+     * @param rowKey  the row key ({@code null} not permitted).
+     * @param columnKey  the column key ({@code null} not permitted).
      * 
-     * @return The value (possibly <code>null</code>).
+     * @return The value (possibly {@code null}).
      */
     T getValue(Comparable<?> seriesKey, Comparable<?> rowKey, 
             Comparable<?> columnKey);
