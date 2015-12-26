e5fada3233bd3ac3848695efa2f24b373c1b2d76
David Gilbert <dave@jfree.org>
2015-12-26 12:56:48 +0100

    Javadoc updates.

--- src/main/java/com/orsoncharts/renderer/xyz/XYZColorSource.java
+++ src/main/java/com/orsoncharts/renderer/xyz/XYZColorSource.java
@@ -35,10 +35,10 @@ package com.orsoncharts.renderer.xyz;
 import java.awt.Color;
 
 /**
- * A source of <code>Color</code> values for an {@link XYZRenderer}.
+ * A source of {@code Color} values for an {@link XYZRenderer}.
  * <p>
  * Classes that implement this interface should also implement 
- * <code>java.io.Serializable</code>, otherwise it will not be possible to 
+ * {@code java.io.Serializable}, otherwise it will not be possible to 
  * serialize and deserialize charts that use the non-serializable instance.
  */
 public interface XYZColorSource {
@@ -49,7 +49,7 @@ public interface XYZColorSource {
      * @param series  the series index.
      * @param item  the item index.
      * 
-     * @return The color (never <code>null</code>). 
+     * @return The color (never {@code null}). 
      */
     Color getColor(int series, int item);
     
@@ -58,7 +58,7 @@ public interface XYZColorSource {
      * 
      * @param series  the series index.
      * 
-     * @return The color (never <code>null</code>).
+     * @return The color (never {@code null}).
      */
     Color getLegendColor(int series);
 
