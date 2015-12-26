5b5ae422d89b2283f11e2da55a797a65a09981e1
David Gilbert <dave@jfree.org>
2015-12-26 10:55:21 +0100

    Javadoc updates.

--- src/main/java/com/orsoncharts/data/Values3D.java
+++ src/main/java/com/orsoncharts/data/Values3D.java
@@ -35,9 +35,11 @@ package com.orsoncharts.data;
 /**
  * A generic representation of a three dimensional grid (cube) of data values.
  * We refer to the indices in the three dimensions as the 
- * <code>seriesIndex</code>, the <code>rowIndex</code> and the 
- * <code>columnIndex</code> (to match the downstream use of this data
+ * {@code seriesIndex}, the {@code rowIndex} and the 
+ * {@code columnIndex} (to match the downstream use of this data
  * structure to represent data values in a three dimensional plot).
+ * 
+ * @param <T> The value type (normally a numeric type).
  * 
  * @see KeyedValues3D
  */
@@ -71,14 +73,14 @@ public interface Values3D<T> {
      * @param rowIndex  the row-index.
      * @param columnIndex  the column-index.
      * 
-     * @return The data value (possibly <code>null</code>).
+     * @return The data value (possibly {@code null}).
      */
     T getValue(int seriesIndex, int rowIndex, int columnIndex);
    
     /**
      * Returns the data item at the specified position as a double primitive.
      * Where the {@link #getValue(int, int, int) } method returns 
-     * <code>null</code>, this method returns <code>Double.NaN</code>.
+     * {@code null}, this method returns {@code Double.NaN}.
      * 
      * @param seriesIndex  the series index.
      * @param rowIndex  the row index.
